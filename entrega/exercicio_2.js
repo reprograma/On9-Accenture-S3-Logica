da318fc5b968d555db5a8505d27030def8d7d87c
cyntiagoulart <cyntiartg@gmail.com>
2020-08-24 14:38:50 -0300

    Entrega do exercicio obrigatório, exercício 2

---
+++ entrega/exercicio_2.js
@@ -0,0 +1,20 @@
+
+"use strict;";
+
+const readlineSync = require("readline-sync");
+
+function inverteTexto() {
+  let texto = readlineSync.question("Digite um texto, por favor: ");
+  let invertido = texto.split("").reverse().join("");
+  console.log(`O texto ${texto} invertido é ${invertido}.`);
+}
+
+inverteTexto();
+
+/* O método split("") transforma o texto num array de caracteres únicos 
+e por ter sido tranformado em array pode-se aplicar métodos de array, que 
+nesse caso são o reverse() para inverter o array e join("") para transformar
+o array em string novamente.*/
+
+/* Nada disso foi explicado em aula. Os fundamentos foram passados superficialmente
+e métodos não foram contemplados em nenhum momento. */
