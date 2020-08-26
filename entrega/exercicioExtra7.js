b5f11095edfbfbe4c606ae5a972c4dcb52b7d723
Maria <meduardalima88@gmail.com>
2020-08-25 22:32:11 -0300

    Exericicio extra 2 concluido apos descoberta da funcao repeat

--- entrega/exercicioExtra7.js
+++ entrega/exercicioExtra7.js
@@ -2,27 +2,14 @@ const readlineSync = require("readline-sync");
 
 
 
-function print (numero) {  
-    let apoio = '';
-    let numeroApoio = numero;
-    let aste ='';
-    let cont = 0;
-
-        while(cont < numero) {
-
-            apoio[cont] += apoio[cont].unshift(' ');
-            cont++;
-
-        }
-
-        for(i=0; i < numero; i++) {
-
-            
-            aste += '*';
-            console.log(apoio[numeroApoio] + aste);
-            numeroApoio--;
-        }
-        
+function print (numero) { 
+    
+    
+    for(let index =1; index<= numero; index++) {
+        console.log(' '.repeat(numero-index)+ '*'.repeat(index));
+    }
+    
+    
         
     
 
