541227dc9880b47923cecb61c858ed5a3d3336ec
Fabí <fabicaneyu@gmail.com>
2020-08-26 13:48:48 -0300

    Exercicio 1 finalizado

--- entrega/exerciciosFabi/exercicio1.js
+++ entrega/exerciciosFabi/exercicio1.js
@@ -4,32 +4,32 @@ Faça um programa que calcule e escreva o número de anos necessários para que 
 mantidas as taxas de crescimento */
 
 // 80000 (100%) 3% a.a
-const paisA = 80000;
-const crescimentoA = 0.03; 
 //const crescimentoA = (paisA*3)/100; 
-
-
 // 200000 (100%) 1.5% a.a
-const paisB = 200000;
-const crescimentoB = 0.015;
 //const crescimentoB = (paisB*1.5)/100; 
 
-let resultA = "";
-let resultB = "";
-let anos = 0;
+
+/*  primeira versão
 
 do {
+  
+  resultA =  paisA +(paisA * 0.03);
+  resultB = paisB +(paisB * 0.015);
   anos = anos + 1;
-  resultA =  paisA +(paisA * crescimentoA);
-  resultB = paisB +(paisB * crescimentoB);
+  
 } while (paisA < paisB);
 
-console.log(resultA);
-console.log(resultB);
+console.log(resultA, resultB, anos); */
+ 
 
+  let paisA = 80000; 
+  let paisB = 200000;
+  anos = 0;
 
-let anos = 0;
-while (paisA < paisB) {
-  console.log(anos);
-  anosr++; 
-}
+ while ( paisA < paisB) {
+  paisA =  paisA +(paisA * 0.03);
+  paisB = paisB +(paisB * 0.015);
+  anos ++;
+ } 
+
+ console.log(anos+" Corresponde ao número de anos necessários para que a população do país A ultrapasse ou iguale a população do país B")
