a7115477053a30c343299600acd4f325a9f3e312
Valeria Melo <valleriademelo@gmail.com>
2020-08-28 00:38:18 -0300

    variables in english

--- entrega/Exercicio.js
+++ entrega/Exercicio.js
@@ -3,13 +3,13 @@
 //1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a população do
 //país A ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento
 
-let paisA = 80000;
-let paisB = 200000;
-let anos = 0;
+let countryA = 80000;
+let countryB = 200000;
+let years = 0;
 
-while (paisA < paisB) {
-  paisA += (paisA * 3) / 100;
-  paisB += (paisB * 1.5) / 100;
-  anos++;
-  console.log('Total de anos: ' + anos);
+while (countryA < countryB) {
+  countryA += (countryA * 3) / 100;
+  countryB += (countryB * 1.5) / 100;
+  years++;
+  console.log('Total de anos: ' + years);
 }
