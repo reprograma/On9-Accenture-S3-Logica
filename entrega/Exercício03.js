264585257e054699229425667e1b5a1bd3e31590
gloriateodoro <gloriateodoro1@gmail.com>
2020-08-29 19:02:52 -0300

    Exercício 03
    Adicionando o exercício que mostra o número de Fibonacci equivalente à posição digitada pelo usuário.

---
+++ entrega/Exercício03.js
@@ -0,0 +1,13 @@
+const readlineSync = require("readline-sync");
+function calculaFibonacci(n) {
+if (n <= 2) return n;
+return calculaFibonacci(n - 1) + calculaFibonacci(n - 2);
+}
+function mostraFibonacci() {
+let numero = readlineSync.question("Digite um número inteiro: ");
+let resultado = calculaFibonacci(parseInt(numero));
+
+console.log("O Fibonacci de " + numero + " equivale a " + resultado);
+}
+
+mostraFibonacci();
