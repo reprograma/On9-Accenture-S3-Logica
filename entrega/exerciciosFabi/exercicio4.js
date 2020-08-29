92b479494a6d1969e8482d214c8d2b0d5e2f53ed
Fabi <fabicaneyu@gmail.com>
2020-08-29 00:41:44 -0300

    Iniciado exercicio 4

--- entrega/exerciciosFabi/exercicio4.js
+++ entrega/exerciciosFabi/exercicio4.js
@@ -2,21 +2,22 @@
    - A função deve receber 3 parâmetros
    - Se qualquer um dos três parâmetros não estiverem preenchidos, a função deve retornar a string: "Preencha todos os valores corretamente!"
    - O retorno da função deve ser a multiplicação dos 3 parâmetros, somando `2` ao resultado da multiplicação. */
+ 
+    const readlineSync = require("readline-sync");
 
-   const readlineSync = require("readline-sync");
+   let num = Number(readlineSync.question("Digite um número: "));
+   let num1 = Number(readlineSync.question("Digite um segundo número: "));
+   let num2 = Number(readlineSync.question("Digite um terceiro número: ")); 
 
-   const num = Number(readlineSync.question("Digite um número: "));
-   const num1 = Number(readlineSync.question("Digite um segundo número: "));
-   const num2 = Number(readlineSync.question("Digite um terceiro número: "));
 
-   funcao teste() {
+   function teste(objeto) {
+      objeto.num = true;
+      objeto.num1 = true;
+      objeto.num2 = true;
       
-      while (
-         if (num === false || num1 === false || num2 === false){
-            console.log("Preencha todos os valores corretamente!")
-         }
-      )
-       else {
-         console.log("Resultado final : "+(num * num1 * num2)+2)
+       if ( num1=== false || num1 === false || num2 === false){
+          console.log("Preencha todos os valores corretamente!") 
       }
-   }
+      return  console.log("Resultado final : "+(num * num1 * num2)+2)
+      }
+   
